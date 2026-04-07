4588edbf0ed892f3eaf8bc4b2b820d90b6aef19d
Raghunandan M <raghu.m2k5@gmail.com>
2026-04-07 06:13:27 +0530

    Delete openenv_customer_support directory

--- openenv_customer_support/app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from openenv.core.env_server import create_fastapi_app
-from environment import ResourceEnv
-
-app: FastAPI = create_fastapi_app(ResourceEnv)
--- openenv_customer_support/app/tasks.py
+++
@@ -1,5 +0,0 @@
-TASKS = [
-    {"id": "easy", "description": "Classify simple email"},
-    {"id": "medium", "description": "Classify + correct action"},
-    {"id": "hard", "description": "Classify + action + meaningful reply"},
-]
